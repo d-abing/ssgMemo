bfd2b6b58cd115355230053c4084019e5a4ad4a1
choikwonwoo <ckw0825@naver.com>
2022-12-18 21:22:34 +0900

    Function : 메인페이지 스와이프 기능

--- app/src/main/java/com/example/ssgmemo/MainActivity.kt
+++ app/src/main/java/com/example/ssgmemo/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.ssgmemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.ssgmemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.SampleLayoutView1.setOnTouchListener(@SuppressLint("ClickableViewAccessibility")
+        object: OnSwipeTouchListener(this@MainActivity) {
+            override fun onSwipeLeft() {
+                Toast.makeText(this@MainActivity,"왼쪽으로",Toast.LENGTH_SHORT).show()
+            }
+            override fun onSwipeRight() {
+                Toast.makeText(this@MainActivity,"오른쪽으로",Toast.LENGTH_SHORT).show()
+            }
+            @SuppressLint("ClickableViewAccessibility")
+            override fun onSwipeTop() {
+                Toast.makeText(this@MainActivity,"위로",Toast.LENGTH_SHORT).show()
+            }
+            override fun onSwipeBottom() {
+                Toast.makeText(this@MainActivity,"아래로",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
