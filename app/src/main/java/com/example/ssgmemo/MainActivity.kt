962a00c725a56d8aa03677c9fa0e1fa26978467f
choikwonwoo <ckw0825@naver.com>
2022-12-18 22:01:38 +0900

    Function : 메인페이지 이미지 이동 기능

--- app/src/main/java/com/example/ssgmemo/MainActivity.kt
+++ app/src/main/java/com/example/ssgmemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ssgmemo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.Toast
 import com.example.ssgmemo.databinding.ActivityMainBinding
 
@@ -12,6 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var startX = 0f
+        var startY = 0f
+
+        binding.imageView.setOnTouchListener { v, event ->
+            when (event.action) {
+
+                MotionEvent.ACTION_DOWN -> {
+                    startX = event.x
+                    startY = event.y
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    val movedX: Float = event.x - startX
+                    val movedY: Float = event.y - startY
+
+                    v.x = v.x + movedX
+                    v.y = v.y + movedY
+                }
+            }
+            true
+        }
+
         binding.SampleLayoutView1.setOnTouchListener(@SuppressLint("ClickableViewAccessibility")
         object: OnSwipeTouchListener(this@MainActivity) {
             override fun onSwipeLeft() {
