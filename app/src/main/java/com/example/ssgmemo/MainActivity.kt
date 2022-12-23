8be03478d3a8fdb90174f5579abda9f7325f2ecd
d-abing <ynlio117@naver.com>
2022-12-23 21:08:16 +0900

    메인_다빈버전

--- app/src/main/java/com/example/ssgmemo/MainActivity.kt
+++ app/src/main/java/com/example/ssgmemo/MainActivity.kt
@@ -1,19 +1,12 @@
 package com.example.ssgmemo
 
-import android.annotation.SuppressLint
-import android.content.ClipData
-import android.content.ClipDescription
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.DragEvent
 import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.ssgmemo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,108 +15,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.top.setOnDragListener(dragListener)
-        binding.bottom.setOnDragListener(dragListener)
-        binding.left.setOnDragListener(dragListener)
-        binding.right.setOnDragListener(dragListener)
-        binding.imageView.setOnLongClickListener {
-            val clipText = "this is our ClipData text"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
-
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
-        }
         var startX = 0f
         var startY = 0f
 
+        binding.memomo.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    startX = event.x
+                    startY = event.y
+                    Log.d("start xy","${startX},${startY}")
+                }
 
-//        binding.imageView.setOnTouchListener(
-//        object: OnSwipeTouchListener(this@MainActivity) {
-//            override fun onSwipeLeft() {
-//                Toast.makeText(this@MainActivity,"왼쪽으로",Toast.LENGTH_SHORT).show()
-//            }
-//            override fun onSwipeRight() {
-//                Toast.makeText(this@MainActivity,"오른쪽으로",Toast.LENGTH_SHORT).show()
-//            }
-//            override fun onSwipeTop() {
-//                Toast.makeText(this@MainActivity,"위로",Toast.LENGTH_SHORT).show()
-//            }
-//            override fun onSwipeBottom() {
-//                Toast.makeText(this@MainActivity,"아래로",Toast.LENGTH_SHORT).show()
-//            }
-//        })
+                MotionEvent.ACTION_MOVE -> {
+                    val movedX:Float= event.x - startX
+                    val movedY:Float= event.y - startY
 
-
-//        binding.imageView.setOnTouchListener { v, event ->
-//            when (event.action) {
-//                MotionEvent.ACTION_DOWN -> {
-//                    startX = event.x
-//                    startY = event.y
-//                    Log.d("start xy","${startX},${startY}")
-//                }
-//
-//                MotionEvent.ACTION_MOVE -> {
-//                    val movedX:Float= event.x - startX
-//                    val movedY:Float= event.y - startY
-//
-//                    v.x = v.x + movedX
-//                    v.y = v.y + movedY
-//                    Log.d("event xy","${v.x},${v.y}")
-//                }
-//                MotionEvent.ACTION_UP -> {
-//                    if(v.x > 500){
-//                        Toast.makeText(this@MainActivity,"오른쪽으로",Toast.LENGTH_SHORT).show()
-//                    }
-//                    if(v.x<0){
-//                        Toast.makeText(this@MainActivity,"왼쪽으로",Toast.LENGTH_SHORT).show()
-//                    }
-////                    v.x =  v.x - (event.x - startX)
-////                    v.y =  v.y - (event.y - startY)
-////                    Log.d("up xy","${event.x},${startX}")
-//
-//                }
-//            }
-//            true
-//        }
-    }
-    val dragListener = View.OnDragListener{view, event ->
-        when(event.action){
-            DragEvent.ACTION_DRAG_STARTED ->{
-                event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                    v.x = v.x + movedX
+                    v.y = v.y + movedY
+                    Log.d("event xy","${v.x},${v.y}")
+                }
+                MotionEvent.ACTION_UP -> {
+                    if(v.x > 500){
+                        Toast.makeText(this@MainActivity,"오른쪽으로",Toast.LENGTH_SHORT).show()
+                    }
+                    if(v.x < 0){
+                        Toast.makeText(this@MainActivity,"왼쪽으로",Toast.LENGTH_SHORT).show()
+                    }
+                    if(v.y > 1147){
+                        Toast.makeText(this@MainActivity,"아래로",Toast.LENGTH_SHORT).show()
+                    }
+                    if(v.y < 463){
+                        Toast.makeText(this@MainActivity,"위로",Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            DragEvent.ACTION_DRAG_ENTERED ->{
-                view.invalidate()
-                true
-            }
-            DragEvent.ACTION_DRAG_LOCATION -> true
-            DragEvent.ACTION_DRAG_EXITED ->{
-                view.invalidate()
-                true
-            }
-            DragEvent.ACTION_DROP ->{
-                val item = event.clipData.getItemAt(0)
-                val dragData = item.text
-                Toast.makeText(this,dragData,Toast.LENGTH_SHORT).show()
-
-                val v = event.localState as View
-                val owner = v.parent as ViewGroup
-                owner.removeView(v)
-                val destination = view as LinearLayout
-                destination.addView(v)
-                v.visibility = View.VISIBLE
-                true
-            }
-            DragEvent.ACTION_DRAG_ENTERED ->{
-                view.invalidate()
-                true
-            }
-            else -> false
+            true
         }
     }
-
 }
